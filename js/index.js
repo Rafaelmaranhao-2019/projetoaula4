18809667e864dc37e2a92b9cd3435d8e84975d6e
TÉCNICO EM INFORMÁTICA 2019.1 <36121742019.1@senacrjedu.local>
2019-08-06 11:40:39 -0300

    acabou

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/*folha1*/
+
+console.log('aplicacao inicial.');
+
+
+
+var img = document.getElementsBytagName('img')[0];
+   document.addEventListener(img, 'click', function{
+   alert('clicou');
+}, false);
+
+
+
+var nav = document.getElementsBytagName('nav')[0];
+img.addEventListener('click') , function(){
+
+
+
+    
+} 
